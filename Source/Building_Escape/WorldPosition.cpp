c38505efc5c44571c8a5e078798fba88f5f7b227
Robertstein <robertovenezia95@gmail.com>
2020-08-14 19:05:52 +0200

    Project initialization
    Unreal Project Setup

---
+++ Source/Building_Escape/WorldPosition.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldPosition.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//FString Log = TEXT("Hello");
+	//FString *PtrLog = &Log;
+
+	//Log.Len();
+	//PtrLog->Len();
+
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), **PtrLog);		//devo usare due asterischi perchè per le stringhe * è overloaded, 
+	//														//quindi in realtà il primo è il pointer, il secondo è l'overloaded operator
+
+	FString ObjName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("The name is %s"), *GetOwner()->GetName());
+	FString ObjPosition = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("The Location is %s"), *ObjPosition);
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
